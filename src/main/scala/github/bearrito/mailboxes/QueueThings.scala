a96cd1150f75ba2587119fb84bfb0d5358da0a9e
bearrito <bstrausser@poweredanalytics.com>
2014-04-27 11:39:17 -0400

    Adds a comment on section I am unsure off

--- src/main/scala/github/bearrito/mailboxes/QueueThings.scala
+++ src/main/scala/github/bearrito/mailboxes/QueueThings.scala
@@ -11,6 +11,12 @@ class ForgetfulQueue[A](capacity: Int) {
     innerQueue.pollFirst()
   }
   def enqueue(e : A) = {
+    // Not sure if this respects any constraints akka makes about message order.
+    // Thread 1 tries to offer last is refused. Enters in loop.
+    // Polls the first. Queue is now below capacity.
+    // Thread 2 offers and and is accepted
+    // Thread 1 offers is not accepted re-enters loops and polls
+    // Thread 1 offers and is accepted.
       while(!innerQueue.offerLast(e)){
         innerQueue.pollFirst()
       }
